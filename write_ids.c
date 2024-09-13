cb77ad48b859d659f60b5fd0003c86c7f271cbcf
Kiayn Sadoughi <ksadoughi1@gmail.com>
2024-09-12 20:38:10 -0400

    i did it

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "kiaynsadoughi";
+    char STUDENT_ID[] = "961023373";
 
     return 0 ;
 }
